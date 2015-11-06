bf018e93cb425c680c2e8a6293bc9bb427d80d0a
Aaron Liberatore <aaron.liberatore@gmail.com>
2015-11-05 19:26:17 -0500

    removes beers example files

--- beers/admin.py
+++
@@ -1,9 +0,0 @@
-from beers.models import Bar, Beer, Drinker, Frequents, Serves, Likes
-from django.contrib import admin
-
-admin.site.register(Bar)
-admin.site.register(Beer)
-admin.site.register(Drinker)
-admin.site.register(Frequents)
-admin.site.register(Serves)
-admin.site.register(Likes)
