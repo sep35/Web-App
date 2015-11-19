ce7287e6456805de72af17b4747b4541423dd006
Aaron Liberatore <aaron.liberatore@gmail.com>
2015-11-18 19:27:38 -0500

    Website runs: login/logout

--- running_log/log/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from .models import Users
-
-def index(request):
-    output = ', '.join([p.name for p in Users.objects.all()])
-    return HttpResponse(output)
